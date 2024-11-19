b5ee1e95708dc9f6637b7d60657b57a1357c1ede
lucasabad <lucasabad80@gmail.com>
2024-11-19 08:43:07 -0300

    Agrego ejercicios con forEach y for of

--- clase07/JS/app.js
+++ clase07/JS/app.js
@@ -1,4 +1,4 @@
-/*function mayorQue (numero){
+function mayorQue (numero){
     return(resultado) => {
 
         if(resultado > numero){
@@ -13,7 +13,7 @@
 
 let mayorQueDiez = mayorQue(10);
 
-console.log(mayorQueDiez(2));*/
+console.log(mayorQueDiez(2));
 
 function operaciones (resultadoOp){
    
@@ -33,3 +33,49 @@ const suma = operaciones("sumar");
 const resta = operaciones("restar");
 
 console.log(suma(2,2));
+
+
+
+
+//SINTAXIS COMPLICADA
+// Esta funcion recibe 2 parametros, el array y el mensajito o funcion especifica
+//Dentro hay un for que recorre todo el array por cada elemento 
+//en este caso imprime cada objeto del array
+//la variable objeto se declara dentro del for
+//Palabras claves for, of
+function porCadaUno(arr, fun){
+    for(const objeto of arr){
+        fun(objeto)
+    }
+}
+//Creo un array con numeros
+const numeritos = [1,2,3,4,5,6];
+//Creo un array con cadena vacia, luego pusheo nuevos elementos
+multi = [];
+//imprimo todo el for y sus elementos
+porCadaUno(numeritos, console.log);
+//en el print creo una funcion dentro del objeto donde agarro el array vacio y pusheo el objeto multiplicado por 2
+porCadaUno(numeritos, objeto => 
+                        { multi.push(objeto * 2 )} );
+//Imprimo el nuevo array "multi"
+console.log(multi);
+
+const jugadores =   [{nombre: "Lucas",valoracion: 89},
+                    {nombre: "Nestor", valoracion: 98},
+                    {nombre:"Facundo", valoracion:67},
+                    {nombre: "Pilo", valoracion:80},
+                    {nombre: "Armando", valoracion:60},
+                    {nombre: "Victor C", valoracion:84}]
+
+jugadores.forEach(
+    jugadores => { 
+        if(jugadores.valoracion > 90){
+            console.log("este jugador juega de puta madre => ", jugadores)
+        }
+    }
+)
+
+jugadores.push({nombre: "Brindisi", valoracion: 99});
+jugadores.push({nombre: "Housseman", valoracion: 101});
+console.log(jugadores);
+
