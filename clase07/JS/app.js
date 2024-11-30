8e39a93b5bd3bbf8c5a6b6997f950c683b187375
lucasabad <lucasabad80@gmail.com>
2024-11-30 14:47:26 -0300

    Agrego ejemplos de funciones de busqueda

--- clase07/JS/app.js
+++ clase07/JS/app.js
@@ -80,3 +80,64 @@ jugadores.push({nombre: "Brindisi", valoracion: 99});
 jugadores.push({nombre: "Housseman", valoracion: 101});
 console.log(jugadores);
 
+//Metodos de busqueda - Funciones de orden superior
+//FOREACH 
+const numeros = [1,2,3,4,5,6];
+numeros.forEach( (el) => {
+    console.log(el);
+})
+
+//Find
+//Se detiene al encontrar el primer valor que cumpla con la condicion dada y lo devuelve
+const Pirania = [{nombre: "Lucas", valoracion: 99, posicion: "LD"}, 
+                 {nombre: "Rodri", valoracion: 80, posicion: "MD"},
+                 {nombre: "Polaco", valoracion: 90, posicion: "DEF"},
+                 {nombre: "Naza", valoracion: 71, posicion: "LI"},
+                 {nombre: "Tuki", valoracion: 40, posicion: "EI"},
+                 {nombre: "Branqui", valoracion: 99, posicion: "MDC"}]
+
+const scouting = Pirania.find((el) => el.valoracion === 99);
+console.log(scouting);
+
+//Filter
+//Devuelve todos los objetos a diferencia del find
+const subeDescuento = [{ PersonaJuridica : "Bna", Descuento: 100},
+                       {PersonaJuridica : "Lemon", Descuento: 60},
+                       {PersonaJuridica : "Uala", Descuento: 40},
+                       {PersonaJuridica : "Modo", Descuento: 80},
+                       {PersonaJuridica : "Prex", Descuento: 20},
+                      ]
+const encuesta = subeDescuento.filter((el) => el.Descuento > 50);
+//Si no pongo json stringify me devuelve object 
+console.log(`Estos son los que tienen mejores ofertas ${JSON.stringify(encuesta)}`);
+         
+//Some
+//Devuelve true o false depende si esta o no en el array
+const ingredientes = [{producto: "Chocolinas"},
+                      {producto: "Chocolate rayado"},
+                      {producto: "Medicreem"},
+                      {producto: "Nutella"}];
+
+console.log( ingredientes.some((el) => el.producto === "Chocolinas"));
+//true
+console.log( ingredientes.some((el) => el.producto === "Chocolinas "));
+//false
+
+//Map 
+//Devuelve todos los atributos pero solo 1 en este caso, que son los nombres de los objetos del array
+const barradeboca = [{nombre : "El abuelo", rango: "Lider"},
+                     {nombre : "Di zeo", rango: "Jefe"},
+                     {nombre : "Mauro martin", rango: "Jefe"},
+                     {nombre : "Manco aravena", rango: "subjefe"},
+                     {nombre : "El oso", rango: "soldado"},
+                     {nombre : "Manzano", rango: "solado"}];
+const liderazgo = barradeboca.map((el) => el.nombre)
+console.log(liderazgo);
+
+//Reduce 
+//En este ejemplo usamos el array de acumulador inicializandolo en 0, y sumamos los elementos
+const precios = [12000, 32000, 14000]
+const totalprecios = precios.reduce((el,precios) => el + precios);
+console.log(totalprecios);
+
+
