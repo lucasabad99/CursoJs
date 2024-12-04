eeeb40dff1058e0ce365cab8412c5d8f5872f2af
lucasabad <lucasabad80@gmail.com>
2024-12-04 20:19:26 -0300

    Agrego ejemplos con localStorage y sessionStorage

---
+++ clase11/JS/app.js
@@ -0,0 +1,8 @@
+localStorage.setItem("nombre","Juan");
+localStorage.setItem("apellido","Perez");
+
+sessionStorage.setItem("nombre","JuanSession");
+sessionStorage.setItem("apellido","PerezSession");
+
+//console.log(sessionStorage.getItem("nombre"));
+//console.log(localStorage.getItem("nombre"));
